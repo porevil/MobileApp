1f6c8b1984b66b544e4c7a796ab494219de1533c
porevil <porevil@Natapongs-MacBook-Air.local>
2014-11-16 23:19:02 +0700

    Feed Me sample Project

---
+++ Playground/MyPlayground.playground/section-1.swift
@@ -0,0 +1,14 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+enum Day : Int{
+    case Monday=1,Thuesday,Wednesday
+}
+
+Day.Monday.rawValue
+
+Day.Monday.hashValue
+
